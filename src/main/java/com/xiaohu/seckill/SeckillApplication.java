a876de8af9760fa7e825c85cc5f3cfea2b30d2c4
xiaohu <1145302691@qq.com>
2022-11-06 17:09:13 +0800

    first commit

---
+++ src/main/java/com/xiaohu/seckill/SeckillApplication.java
@@ -0,0 +1,18 @@
+package com.xiaohu.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 陈晓虎
+ * @className SeckillApplication
+ * @description TODO
+ * @email 1145302691@qq.com
+ * @date 2022/11/6 15:00
+ **/
+@SpringBootApplication
+public class SeckillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}
